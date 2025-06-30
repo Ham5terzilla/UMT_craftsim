71df77153b252b2bc6acfc7f5124b69568076c70
Ham5terzilla <mmmm456456@mail.ru>
2025-06-30 14:09:21 +0500

    fix: Finished transformation_registrty module
    Finished unfinished parts in transformation_registry module. Added docstrings.

--- crafting_system/transformations/transformation_registry.py
+++ crafting_system/transformations/transformation_registry.py
@@ -1,6 +1,5 @@
 """
-module with registry class for transformation. It works, but I'm not sure what to do with that, currently seems too complicated.\n
-In case if someone would use it, need to finish TransformationRegistry.registry dict
+Central registry for machine-to-transformation mappings.
 """
 
 from crafting_system.constants import Machines
@@ -10,50 +9,98 @@ from crafting_system.transformations.transformations_single import Transformatio
 
 
 class TransformationRegistry:
-    """
-    unfinished, purpose of this class is self.registry dict with keys as objects from Machines and tranformations from corresponding modules\n
-    contains:
-        self.registry: dict[Machines, transformation_single | transformation_multiple | None]
-        get_transformation method, which input str or constant from Machines and returns corresponding transformation class
+    """Central catalog mapping crafting machines to their transformation logic.
+    
+    Attributes:
+        registry (dict[Machines, Transformation_Single | Transformation_Multiple | None]):
+            Complete mapping of all machines to their corresponding transformation classes.
+            None indicates unimplemented transformations.
+    
+    ## Methods
+        get_transformation: Retrieves transformation class for a given machine
+    
+    Example:
+        frame_maker = TransformationRegistry.get_transformation("Frame Maker")
+        frame = frame_maker().transform(bar, bolts)
     """
 
     registry = {
-        Machines.BAR_TO_GEM_TRANSMUTER: transformations_single.BTGTransformation,
-        Machines.BLAST_FURNACE: transformations_single.BlastFurnaceTransformation,
-        Machines.BOLT_MACHINE: transformations_single.BoltMachineTransformation,
-        Machines.CERAMIC_FURNACE: transformations_single.CeramicFurnaceTransformation,
-        Machines.BRICK_MOLD: None,
-        Machines.COILER: transformations_single.CoilerTransformation,
-        Machines.CRUSHER: transformations_single.CrusherTransformation,
+        Machines.ORE_CLEANER: transformations_single.OreCleanerTransformation,
+        Machines.POLISHER: transformations_single.PolisherTransformation,
+        Machines.ELECTRONIC_TUNER: transformations_single.PolisherTransformation,
+        Machines.GEM_CUTTER: transformations_single.GemCutterTransformation,
+        Machines.TEMPERING_FORGE: transformations_single.TemperingForgeTransformation,
+        Machines.QUALITY_ASSURANCE_MACHINE: transformations_single.QAMachineTransformation,
+        Machines.PHILOSOPHERS_STONE: transformations_single.PhilosophersStoneTransformation,
+        Machines.ORE_UPGRADER: transformations_single.OreUpgraderTransformation,
+        Machines.TOPAZ_PROSPECTOR: None,
+        Machines.EMERALD_PROSPECTOR: None,
+        Machines.SAPPHIRE_PROSPECTOR: None,
+        Machines.RUBY_PROSPECTOR: None,
         Machines.DIAMOND_PROSPECTOR: None,
-        Machines.ALLOY_FURNACE: transformations_multiple.AlloyFurnaceTransformation,
-        Machines.AMULET_MAKER: transformations_multiple.AmuletMakerTransformation,
-        Machines.BLASTING_POWDER_CHAMBER: transformations_multiple.BlastingPowderChamberTransformation,
-        Machines.BLASTING_POWDER_REFINER: transformations_multiple.BlastingPowderRefinerTransformation,
-        Machines.CASING_MACHINE: transformations_multiple.CasingMachineTransformation,
+        Machines.ORE_SMELTER: transformations_single.OreSmelterTransformation,
+        Machines.CRUSHER: transformations_single.CrusherTransformation,
+        Machines.COILER: transformations_single.CoilerTransformation,
+        Machines.BRICK_MOLD: None,
+        Machines.BOLT_MACHINE: transformations_single.BoltMachineTransformation,
+        Machines.PLATE_STAMPER: transformations_single.PlateStamperTransformation,
+        Machines.SIFTER: None,
+        Machines.PIPE_MAKER: transformations_single.PipeMakerTransformation,
+        Machines.KILN: None,
+        Machines.MECHANICAL_PARTS_MAKER: transformations_single.MechanicalPartsMakerTransformation,
+        Machines.BLAST_FURNACE: transformations_single.BlastFurnaceTransformation,
+        Machines.CERAMIC_FURNACE: transformations_single.CeramicFurnaceTransformation,
+        Machines.FILIGREE_CUTTER: transformations_single.FiligreeCutterTransformation,
+        Machines.LENS_CUTTER: transformations_single.LensCutterTransformation,
+        Machines.DUPLICATOR: transformations_single.DuplicatorTransformation,
+        Machines.NANO_SIFTER: None,
+        Machines.GEM_TO_BAR_TRANSMUTER: transformations_single.GTBTransformation,
+        Machines.BAR_TO_GEM_TRANSMUTER: transformations_single.BTGTransformation,
         Machines.CEMENT_MIXER: None,
+        Machines.FRAME_MAKER: transformations_multiple.FrameMakerTransformation,
+        Machines.RING_MAKER: transformations_multiple.RingMakerTransformation,
+        Machines.BLASTING_POWDER_CHAMBER: transformations_multiple.BlastingPowderChamberTransformation,
+        Machines.EXPLOSIVES_MAKER: transformations_multiple.ExplosivesMakerTransformation,
         Machines.CIRCUIT_MAKER: transformations_multiple.CircuitMakerTransformation,
         Machines.CLAY_MIXER: transformations_multiple.ClayMixerTransformation,
-        Machines.DUPLICATOR: transformations_single.DuplicatorTransformation,
+        Machines.CASING_MACHINE: transformations_multiple.CasingMachineTransformation,
+        Machines.PRISMATIC_GEM_CRUCIBLE: transformations_multiple.PrismaticGemCrucibleTransformation,
+        Machines.ALLOY_FURNACE: transformations_multiple.AlloyFurnaceTransformation,
+        Machines.MAGNETIC_MACHINE: transformations_multiple.MagneticMachineTransformation,
+        Machines.OPTICS_MACHINE: transformations_multiple.OpticsMachineTransformation,
+        Machines.GILDER: transformations_multiple.GilderTransformation,
+        Machines.ENGINE_FACTORY: transformations_multiple.EngineFactoryTransformation,
+        Machines.SUPERCONDUCTOR_CONSTRUCTOR: transformations_multiple.SuperconductorConstructorTransformation,
+        Machines.AMULET_MAKER: transformations_multiple.AmuletMakerTransformation,
+        Machines.TABLET_FACTORY: transformations_multiple.TabletFactoryTransformation,
+        Machines.BLASTING_POWDER_REFINER: transformations_multiple.BlastingPowderRefinerTransformation,
+        Machines.LASER_MAKER: transformations_multiple.LaserMakerTransformation,
+        Machines.POWER_CORE_ASSEMBLER: transformations_multiple.PowerCoreAssemblerTransformation,
     }
 
     @classmethod
     def get_transformation(
         cls, machine_name: str | Machines
     ) -> Transformation_Multiple | Transformation_Single:
-        """Get corresponding transformation class from name of machine, based on dict \n
-        with keys equals to keys in constants.Machines and values equals classes in transformations_single.py and *_multiple.py
+        """Retrieves transformation class for a crafting machine.\n
+        Provides machine name resolution from both string identifiers and enum members.
 
         Args:
-            machine_name (str | Machines): can be directly part of Machines class or key of Machines, as used as key in TransformationRegistry.registry dict
+            machine_name (str | Machines): Machine identifier (case-insensitive string or Machines enum)
 
         Raises:
-            KeyError: if machine with name machine_name not found in constants.Machines
-            KeyError: if transformation for machine with machine_name not found
+            KeyError: For invalid/unregistered machine names
             NotImplementedError: if transformation for machine is found, but not implemented
 
         Returns:
-            Transformation class corresponding to machine if found successful
+            Transformation class (callable) for the specified machine
+        
+        Example:
+            Using enum\n
+            smelter = TransformationRegistry.get_transformation(Machines.ORE_SMELTER)
+            
+            Using string\n
+            cleaner = TransformationRegistry.get_transformation("Ore Cleaner")
         """
 
         if isinstance(machine_name, str):
